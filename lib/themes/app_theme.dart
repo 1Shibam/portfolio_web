e76149798594763481ecbc28b67e6f79be071a9c
1Shibam <hehe.shibam@gmail.com>
2025-02-23 17:08:53 +0530

    app theme changes

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -3,67 +3,67 @@ import 'package:portfolio_web/themes/colors.dart';
 import 'package:portfolio_web/themes/text_styles.dart';
 
 class AppTheme {
-  //! Light Theme
-
-  static final ThemeData lightTheme = ThemeData(
+  // Light Theme
+  static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
+    fontFamily: 'Freeman',
     scaffoldBackgroundColor: LightThemeColors.background,
     primaryColor: LightThemeColors.primary,
-    cardColor: LightThemeColors.card,
-    appBarTheme: AppBarTheme(
-      backgroundColor: LightThemeColors.surface,
-      titleTextStyle: AppTextStyles.headlineSmall.copyWith(color: Colors.white),
-    ),
     textTheme: TextTheme(
-      headlineLarge: AppTextStyles.headlineLarge
-          .copyWith(color: LightThemeColors.textPrimary),
-      headlineMedium: AppTextStyles.headlineMedium
-          .copyWith(color: LightThemeColors.textPrimary),
-      headlineSmall: AppTextStyles.headlineSmall
-          .copyWith(color: LightThemeColors.textSecondary),
-      bodyLarge:
-          AppTextStyles.bodyLarge.copyWith(color: LightThemeColors.textPrimary),
-      bodyMedium: AppTextStyles.bodyMedium
-          .copyWith(color: LightThemeColors.textSecondary),
-      bodySmall: AppTextStyles.bodySmall
-          .copyWith(color: LightThemeColors.textSecondary),
-      labelLarge: AppTextStyles.button.copyWith(color: Colors.white),
+      headlineLarge:
+          AppTextStyles.headlineLarge.copyWith(color: LightThemeColors.text),
+      bodyLarge: AppTextStyles.bodyLarge.copyWith(color: LightThemeColors.text),
+      labelLarge: AppTextStyles.button.copyWith(color: LightThemeColors.text),
     ),
-    buttonTheme: const ButtonThemeData(
-      buttonColor: LightThemeColors.primary,
-      textTheme: ButtonTextTheme.primary,
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: LightThemeColors.primary,
+        textStyle: AppTextStyles.button,
+      ),
     ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: LightThemeColors.primary,
+        foregroundColor: Colors.white,
+        textStyle: AppTextStyles.button,
+      ),
+    ),
+    colorScheme: const ColorScheme.light(
+        surface: LightThemeColors.background,
+        primary: LightThemeColors.primary,
+        secondary: LightThemeColors.secondary,
+        tertiary: LightThemeColors.text),
   );
 
-  //! Dark theme
-  static final ThemeData darkTheme = ThemeData(
+  // Dark Theme
+  static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
+    fontFamily: 'Freeman',
     scaffoldBackgroundColor: DarkThemeColors.background,
     primaryColor: DarkThemeColors.primary,
-    cardColor: DarkThemeColors.card,
-    appBarTheme: AppBarTheme(
-      backgroundColor: DarkThemeColors.surface,
-      titleTextStyle: AppTextStyles.headlineSmall.copyWith(color: Colors.white),
-    ),
     textTheme: TextTheme(
-      headlineLarge: AppTextStyles.headlineLarge
-          .copyWith(color: DarkThemeColors.textPrimary),
-      headlineMedium: AppTextStyles.headlineMedium
-          .copyWith(color: DarkThemeColors.textPrimary),
-      headlineSmall: AppTextStyles.headlineSmall
-          .copyWith(color: DarkThemeColors.textSecondary),
-      bodyLarge:
-          AppTextStyles.bodyLarge.copyWith(color: DarkThemeColors.textPrimary),
-      bodyMedium: AppTextStyles.bodyMedium
-          .copyWith(color: DarkThemeColors.textSecondary),
-      bodySmall: AppTextStyles.bodySmall
-          .copyWith(color: DarkThemeColors.textSecondary),
-      labelLarge: AppTextStyles.button.copyWith(color: Colors.white),
+      headlineLarge:
+          AppTextStyles.headlineLarge.copyWith(color: DarkThemeColors.text),
+      bodyLarge: AppTextStyles.bodyLarge.copyWith(color: DarkThemeColors.text),
+      labelLarge: AppTextStyles.button.copyWith(color: DarkThemeColors.text),
     ),
-    // ignore: prefer_const_constructors
-    buttonTheme: ButtonThemeData(
-      buttonColor: DarkThemeColors.primary,
-      textTheme: ButtonTextTheme.primary,
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: DarkThemeColors.primary,
+        textStyle: AppTextStyles.button,
+      ),
     ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: DarkThemeColors.primary,
+        foregroundColor: Colors.white,
+        textStyle: AppTextStyles.button,
+      ),
+    ),
+    colorScheme: const ColorScheme.dark(
+        surface: DarkThemeColors.background,
+        primary: DarkThemeColors.primary,
+        secondary: DarkThemeColors.secondary,
+        tertiary: DarkThemeColors.text),
   );
 }
