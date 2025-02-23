61507272d3e7266e798bddafa7d85037e1625512
1Shibam <hehe.shibam@gmail.com>
2025-02-23 16:03:56 +0530

    update the router config

--- lib/router/router_config.dart
+++ lib/router/router_config.dart
@@ -1,11 +1,17 @@
 import 'package:go_router/go_router.dart';
-import 'package:portfolio_web/screens/home_page.dart';
+import 'package:portfolio_web/responsive/responsive_layout.dart';
+
+import 'package:portfolio_web/screens/mobile_screen_layout.dart';
+import 'package:portfolio_web/screens/web_screen_layout.dart';
 
 final GoRouter router = GoRouter(initialLocation: '/', routes: [
   GoRoute(
     path: '/',
     builder: (context, state) {
-      return const HomePage();
+      return const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      );
     },
   )
 ]);
