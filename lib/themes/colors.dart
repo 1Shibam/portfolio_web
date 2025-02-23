1c772e302131375eb27370c1b0918ab230921df3
1Shibam <hehe.shibam@gmail.com>
2025-02-23 17:09:00 +0530

    colors chnages

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -1,27 +1,18 @@
 import 'package:flutter/material.dart';
 
 class DarkThemeColors {
-  static const Color background = Color(0xff131919);
-  static const Color surface = Color(0xff152626);
-  static const Color card = Color(0xff1a3534);
-  static const Color accent = Color(0xff1d4544);
-  static const Color primary = Color(0xff215b5a);
-  static const Color secondary = Color(0xff25716f);
-  static const Color highlight = Color(0xff3e837f);
-  static const Color textPrimary = Color(0xff5e9590);
-  static const Color textSecondary = Color(0xff82a4a0);
-  static const Color border = Color(0xffa7b2b0);
+  static const Color background = Color(0xFF121212); // Dark Gray
+  static const Color surface = Color(0xFF1E1E1E); // Slightly lighter gray
+  static const Color primary =
+      Color.fromARGB(255, 134, 193, 252); // Soft purple accent
+  static const Color secondary = Color(0xFF03DAC6); // Subtle teal
+  static const Color text = Color(0xFFE0E0E0); // Light gray
 }
 
 class LightThemeColors {
-  static const Color background = Color(0xffb6c2c0);
-  static const Color surface = Color(0xffaab5b3);
-  static const Color card = Color(0xff85a8a4);
-  static const Color accent = Color(0xff629c97);
-  static const Color primary = Color(0xff438f8b);
-  static const Color secondary = Color(0xff28817f);
-  static const Color highlight = Color(0xff185b5c);
-  static const Color textPrimary = Color(0xff0b3336);
-  static const Color textSecondary = Color(0xff020e0f);
-  static const Color border = Color(0xff000000);
+  static const Color background = Color(0xFFFFFFFF); // Pure White
+  static const Color surface = Color(0xFFF5F5F5); // Soft Gray
+  static const Color primary = Color.fromARGB(255, 15, 165, 228); // Deep purple
+  static const Color secondary = Color(0xFF018786); // Muted teal
+  static const Color text = Color(0xFF212121); // Dark gray for readability
 }
