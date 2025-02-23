ba55368b837924440e9f39ca10b525dac481a229
1Shibam <hehe.shibam@gmail.com>
2025-02-23 17:08:46 +0530

    working on web layout

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,12 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_web/themes/text_styles.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('This is Web Screen my nigga'),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.dashboard,
+              color: Theme.of(context).primaryColor,
+            )
+          ],
+        ),
+        actions: [
+          CustomTextButtons(
+            text: 'About Me',
+            onPressed: () {},
+          ),
+          CustomTextButtons(
+            text: 'Resume',
+            onPressed: () {},
+          ),
+          CustomTextButtons(
+            text: 'Project',
+            onPressed: () {},
+          ),
+          CustomTextButtons(
+            text: 'Contact',
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'This is Web Screen my nigga',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextButtons extends StatelessWidget {
+  final void Function()? onPressed;
+  final String text;
+  const CustomTextButtons({super.key, required this.text, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10.0),
+      child: TextButton(
+          onPressed: () {},
+          child: Text(
+            text,
+            style: TextStyle(color: Theme.of(context).colorScheme.tertiary),
+          )),
     );
   }
 }
