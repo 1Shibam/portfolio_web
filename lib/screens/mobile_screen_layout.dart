cc950716d238491a7543d395db1693beb8ad877e
1Shibam <hehe.shibam@gmail.com>
2025-02-23 17:08:37 +0530

    working on mobile layout

--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_web/themes/text_styles.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({super.key});
@@ -6,7 +7,12 @@ class MobileScreenLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text('This is mobile screen my nigga'),),
+      body: Center(
+        child: Text(
+          'This is mobile screen my nigga',
+          style: AppTextStyles.bodyLarge,
+        ),
+      ),
     );
   }
 }
