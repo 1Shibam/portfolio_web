fe2cde1ec1a3d9812f54fb85ae29d17b05e87f92
1Shibam <hehe.shibam@gmail.com>
2025-02-23 15:11:25 +0530

    integrate firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   runApp(const ProviderScope(child: MyPortfolioWeb()));
 }
 
