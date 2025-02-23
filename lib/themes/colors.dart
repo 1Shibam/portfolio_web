ae168eb0ccd2a911572d2c434cf76e6fe1afd872
1Shibam <hehe.shibam@gmail.com>
2025-02-23 15:43:00 +0530

    adds colors class

---
+++ lib/themes/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class DarkThemeColors {
+  static const Color background = Color(0xff131919);
+  static const Color surface = Color(0xff152626);
+  static const Color card = Color(0xff1a3534);
+  static const Color accent = Color(0xff1d4544);
+  static const Color primary = Color(0xff215b5a);
+  static const Color secondary = Color(0xff25716f);
+  static const Color highlight = Color(0xff3e837f);
+  static const Color textPrimary = Color(0xff5e9590);
+  static const Color textSecondary = Color(0xff82a4a0);
+  static const Color border = Color(0xffa7b2b0);
+}
+
+class LightThemeColors {
+  static const Color background = Color(0xffb6c2c0);
+  static const Color surface = Color(0xffaab5b3);
+  static const Color card = Color(0xff85a8a4);
+  static const Color accent = Color(0xff629c97);
+  static const Color primary = Color(0xff438f8b);
+  static const Color secondary = Color(0xff28817f);
+  static const Color highlight = Color(0xff185b5c);
+  static const Color textPrimary = Color(0xff0b3336);
+  static const Color textSecondary = Color(0xff020e0f);
+  static const Color border = Color(0xff000000);
+}
