66ae0e938476cee0a7e2902553ad1cc30f727f6f
1Shibam <hehe.shibam@gmail.com>
2025-02-23 15:56:43 +0530

    update themes

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -1,39 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_web/themes/colors.dart';
+import 'package:portfolio_web/themes/text_styles.dart';
 
 class AppTheme {
+  //! Light Theme
+
   static final ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     scaffoldBackgroundColor: LightThemeColors.background,
     primaryColor: LightThemeColors.primary,
     cardColor: LightThemeColors.card,
-    appBarTheme: const AppBarTheme(
+    appBarTheme: AppBarTheme(
       backgroundColor: LightThemeColors.surface,
-      titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+      titleTextStyle: AppTextStyles.headlineSmall.copyWith(color: Colors.white),
     ),
-    textTheme: const TextTheme(
-      bodyLarge: TextStyle(color: LightThemeColors.textPrimary),
-      bodyMedium: TextStyle(color: LightThemeColors.textSecondary),
+    textTheme: TextTheme(
+      headlineLarge: AppTextStyles.headlineLarge
+          .copyWith(color: LightThemeColors.textPrimary),
+      headlineMedium: AppTextStyles.headlineMedium
+          .copyWith(color: LightThemeColors.textPrimary),
+      headlineSmall: AppTextStyles.headlineSmall
+          .copyWith(color: LightThemeColors.textSecondary),
+      bodyLarge:
+          AppTextStyles.bodyLarge.copyWith(color: LightThemeColors.textPrimary),
+      bodyMedium: AppTextStyles.bodyMedium
+          .copyWith(color: LightThemeColors.textSecondary),
+      bodySmall: AppTextStyles.bodySmall
+          .copyWith(color: LightThemeColors.textSecondary),
+      labelLarge: AppTextStyles.button.copyWith(color: Colors.white),
     ),
     buttonTheme: const ButtonThemeData(
       buttonColor: LightThemeColors.primary,
       textTheme: ButtonTextTheme.primary,
     ),
   );
+
+  //! Dark theme
   static final ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
     scaffoldBackgroundColor: DarkThemeColors.background,
     primaryColor: DarkThemeColors.primary,
     cardColor: DarkThemeColors.card,
-    appBarTheme: const AppBarTheme(
+    appBarTheme: AppBarTheme(
       backgroundColor: DarkThemeColors.surface,
-      titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+      titleTextStyle: AppTextStyles.headlineSmall.copyWith(color: Colors.white),
     ),
-    textTheme: const TextTheme(
-      bodyLarge: TextStyle(color: DarkThemeColors.textPrimary),
-      bodyMedium: TextStyle(color: DarkThemeColors.textSecondary),
+    textTheme: TextTheme(
+      headlineLarge: AppTextStyles.headlineLarge
+          .copyWith(color: DarkThemeColors.textPrimary),
+      headlineMedium: AppTextStyles.headlineMedium
+          .copyWith(color: DarkThemeColors.textPrimary),
+      headlineSmall: AppTextStyles.headlineSmall
+          .copyWith(color: DarkThemeColors.textSecondary),
+      bodyLarge:
+          AppTextStyles.bodyLarge.copyWith(color: DarkThemeColors.textPrimary),
+      bodyMedium: AppTextStyles.bodyMedium
+          .copyWith(color: DarkThemeColors.textSecondary),
+      bodySmall: AppTextStyles.bodySmall
+          .copyWith(color: DarkThemeColors.textSecondary),
+      labelLarge: AppTextStyles.button.copyWith(color: Colors.white),
     ),
-    buttonTheme: const ButtonThemeData(
+    // ignore: prefer_const_constructors
+    buttonTheme: ButtonThemeData(
       buttonColor: DarkThemeColors.primary,
       textTheme: ButtonTextTheme.primary,
     ),
