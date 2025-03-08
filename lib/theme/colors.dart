d8458b58bb443bc85288fb9b3629a867f224bb36
1Shibam <hehe.shibam@gmail.com>
2025-03-09 01:53:59 +0530

    update colors

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
 
-
 class AppColors {
-  static const Color background = Color(0xFF121212);
-  static const Color surface = Color(0xFF1E1E1E);
-  static const Color primaryText = Color(0xFFE0E0E0);
-  static const Color secondaryText = Color(0xFFB0B0B0);
-  static const Color accentPurple = Color(0xFFBB86FC);
-  static const Color accentCyan = Color(0xFF03DAC6);
-  static const Color accentOrange = Color(0xFFFF5722);
-  static const Color mutedBrown = Color(0xFF8D6E63);
-  static const Color errorRed = Color(0xFFCF6679);
-  static const Color successGreen = Color(0xFF00C853);
+  static const Color surface = Color.fromARGB(255, 7, 0, 29);
+  static const Color background = Color.fromARGB(255, 0, 0, 0);
+  static const Color light = Color.fromARGB(255, 195, 255, 253);
 }
-
