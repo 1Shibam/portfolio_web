a09d763b3892ee38cc890a3351b44f6822af9466
1Shibam <hehe.shibam@gmail.com>
2025-03-09 01:54:11 +0530

    add custom fonts

--- lib/theme/web_theme.dart
+++ lib/theme/web_theme.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_web/theme/colors.dart';
 
-ThemeData webAppTheme = ThemeData();
+ThemeData webAppTheme = ThemeData(
+    scaffoldBackgroundColor: AppColors.background,
+    drawerTheme: DrawerThemeData(
+        backgroundColor: Colors.transparent,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(60),
+            side: const BorderSide(color: AppColors.light))));
