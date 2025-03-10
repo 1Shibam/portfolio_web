259329a47f5e5905304bec5e1c75504091daf6b0
1Shibam <hehe.shibam@gmail.com>
2025-03-11 00:48:06 +0530

    update the theme for scrollbar

--- lib/theme/web_theme.dart
+++ lib/theme/web_theme.dart
@@ -3,6 +3,10 @@ import 'package:portfolio_web/theme/colors.dart';
 
 ThemeData webAppTheme = ThemeData(
     scaffoldBackgroundColor: AppColors.background,
+    scrollbarTheme: ScrollbarThemeData(
+        interactive: true,
+        thumbColor: const WidgetStatePropertyAll(AppColors.light),
+        trackColor: WidgetStatePropertyAll(AppColors.light.withOpacity(0.05))),
     drawerTheme: DrawerThemeData(
         backgroundColor: Colors.transparent,
         shape: RoundedRectangleBorder(
