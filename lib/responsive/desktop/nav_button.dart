f6b331ecce477958a942bd7451bc3c4a0452ab06
1Shibam <hehe.shibam@gmail.com>
2025-03-10 21:26:42 +0530

    update nav buttons for page changes

--- lib/responsive/desktop/nav_button.dart
+++ lib/responsive/desktop/nav_button.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_web/theme/colors.dart';
 import 'package:portfolio_web/theme/text_styles.dart';
 
 class NavButton extends StatelessWidget {
   final String title;
   final VoidCallback onTap;
+  final bool isSelected;
 
-  const NavButton({super.key, required this.title, required this.onTap});
+  const NavButton(
+      {super.key,
+      required this.title,
+      required this.onTap,
+      required this.isSelected});
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +19,18 @@ class NavButton extends StatelessWidget {
       onTap: onTap,
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 12),
-        child: Text(
-          title,
-          style: AppTextStyles.heading,
+        child: Container(
+          decoration: BoxDecoration(
+              color: isSelected
+                  ? Colors.white.withOpacity(0.2)
+                  : Colors.transparent,
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(color: AppColors.light, width: 2)),
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          child: Text(
+            title,
+            style: AppTextStyles.heading,
+          ),
         ),
       ),
     );
