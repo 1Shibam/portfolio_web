7cdd5dd0caf815dd2da31b0a3d5674fc2e64f015
1Shibam <hehe.shibam@gmail.com>
2025-03-11 20:05:36 +0530

    update the project section widget

--- lib/responsive/desktop/project_section_widget.dart
+++ lib/responsive/desktop/project_section_widget.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:portfolio_web/theme/text_styles.dart';
 import 'package:portfolio_web/widgets/section_divide_widget.dart';
 
 class ProjectSectionWidget extends StatefulWidget {
   const ProjectSectionWidget({
     super.key,
-    required this.maxHeight,
-    required this.maxWidth,
   });
-
-  final double maxHeight;
-  final double maxWidth;
 
   @override
   State<ProjectSectionWidget> createState() => _ProjectSectionWidgetState();
@@ -25,35 +22,46 @@ class _ProjectSectionWidgetState extends State<ProjectSectionWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: widget.maxHeight * 0.1,
-        ),
-        Expanded(
-          child: Scrollbar(
-            interactive: true,
-            controller: _scrollController,
-            thickness: widget.maxWidth * 0.01,
-            thumbVisibility: true, // Always show the scrollbar
-            trackVisibility: true,
-            child: ListView.builder(
+    double maxWidth = MediaQuery.of(context).size.width;
+    double maxHeight = MediaQuery.of(context).size.height;
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          horizontal: maxWidth * 0.05, vertical: maxHeight * 0.05),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Projects',
+            style: AppTextStyles.heading(context),
+          ).animate().fade(duration: 500.ms).slideY(),
+          SizedBox(
+            height: maxHeight * 0.02,
+          ),
+          Expanded(
+            child: Scrollbar(
+              interactive: true,
               controller: _scrollController,
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SectionDividerWidget(
-                      maxWidth: widget.maxWidth,
-                      maxHeight: widget.maxHeight,
-                      title1: 'video Source',
-                      title2: 'Tech-Stacks'),
-                );
-              },
+              thickness: maxWidth * 0.01,
+              thumbVisibility: true, // Always show the scrollbar
+              trackVisibility: true,
+              child: ListView.builder(
+                controller: _scrollController,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(right: 0.02 * maxWidth),
+                    child: SectionDividerWidget(
+                        maxWidth: maxWidth,
+                        maxHeight: maxHeight,
+                        title1: 'video Source',
+                        title2: 'Tech-Stacks'),
+                  );
+                },
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
