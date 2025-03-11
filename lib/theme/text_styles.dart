5e2a9915ed682a3a046a2caf9080e93308cb4150
1Shibam <hehe.shibam@gmail.com>
2025-03-11 20:05:59 +0530

    add soem more text styles

--- lib/theme/text_styles.dart
+++ lib/theme/text_styles.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_web/theme/colors.dart';
 
 import 'fonts.dart';
 
 double adaptiveFontSize(BuildContext context, double baseSize) {
   double width = MediaQuery.of(context).size.width;
-  
+
   if (width < 600) {
     return baseSize * 0.9; // Small screens (mobile)
   } else if (width < 1200) {
@@ -14,41 +15,50 @@ double adaptiveFontSize(BuildContext context, double baseSize) {
   }
 }
 
-
 class AppTextStyles {
   static TextStyle normal(BuildContext context) => TextStyle(
         fontFamily: nuraNormal,
         fontWeight: FontWeight.normal,
         fontSize: adaptiveFontSize(context, 16),
-        color: Colors.white,
+        color: AppColors.light,
       );
 
   static TextStyle medium(BuildContext context) => TextStyle(
         fontFamily: nuraMedium,
         fontWeight: FontWeight.w500,
         fontSize: adaptiveFontSize(context, 18),
-        color: Colors.white,
+        color: const Color.fromARGB(255, 165, 215, 214),
       );
 
   static TextStyle bold(BuildContext context) => TextStyle(
         fontFamily: nuraBold,
         fontWeight: FontWeight.bold,
         fontSize: adaptiveFontSize(context, 20),
-        color: Colors.white,
+        color: AppColors.light,
       );
 
   static TextStyle heading(BuildContext context) => TextStyle(
-        fontFamily: nuraBold,
-        fontWeight: FontWeight.bold,
-        fontSize: adaptiveFontSize(context, 32),
-        color: Colors.white,
-      );
+      fontFamily: nuraBold,
+      fontWeight: FontWeight.bold,
+      fontSize: adaptiveFontSize(context, 40),
+      color: AppColors.light);
+  static TextStyle heading2(BuildContext context) => TextStyle(
+      fontFamily: nuraBold,
+      fontWeight: FontWeight.bold,
+      fontSize: adaptiveFontSize(context, 28),
+      color: AppColors.light);
 
   static TextStyle subheading(BuildContext context) => TextStyle(
         fontFamily: nuraMedium,
         fontWeight: FontWeight.w500,
         fontSize: adaptiveFontSize(context, 24),
-        color: Colors.white,
+        color: const Color.fromARGB(255, 165, 215, 214),
+      );
+  static TextStyle subheading2(BuildContext context) => TextStyle(
+        fontFamily: nuraMedium,
+        fontWeight: FontWeight.w500,
+        fontSize: adaptiveFontSize(context, 20),
+        color: const Color.fromARGB(255, 165, 215, 214),
       );
 
   static TextStyle button(BuildContext context) => TextStyle(
@@ -56,6 +66,6 @@ class AppTextStyles {
         fontWeight: FontWeight.bold,
         fontSize: adaptiveFontSize(context, 16),
         letterSpacing: 1.2,
-        color: Colors.white,
+        color: const Color.fromARGB(255, 165, 215, 214),
       );
 }
