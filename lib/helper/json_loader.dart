a1e0f29f7dc3256397aebc33bfe829bd5b92b19d
1Shibam <hehe.shibam@gmail.com>
2025-03-13 00:49:29 +0530

    create json loader class

--- lib/helper/json_loader.dart
+++ lib/helper/json_loader.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portfolio_web/model/data_model.dart';
 
 class JsonLoader {
@@ -10,3 +11,5 @@ class JsonLoader {
     return User.fromJson(jsonData);
   }
 }
+
+final jsonLoaderProvider = Provider<JsonLoader>((ref) => JsonLoader());
