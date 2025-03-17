70b6bbbf4e7b2ffdacc2bfe7855bb05be520a482
1Shibam <hehe.shibam@gmail.com>
2025-03-17 21:20:09 +0530

    updatae the navbutton to animated buttons

--- lib/responsive/nav_buttons_widget.dart
+++ lib/responsive/nav_buttons_widget.dart
@@ -1,48 +1,100 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:portfolio_web/responsive/desktop/desktop_screen_layout.dart';
 import 'package:portfolio_web/responsive/desktop/nav_button.dart';
 import 'package:portfolio_web/theme/colors.dart';
 
-class NavbarButtonsWidget extends ConsumerWidget {
-  const NavbarButtonsWidget({
-    super.key,
-    required this.selectedIndex,
-  });
+class NavbarButtonsWidget extends ConsumerStatefulWidget {
+  const NavbarButtonsWidget({super.key, required this.selectedIndex});
 
   final int selectedIndex;
 
   @override
-  Widget build(BuildContext context, ref) {
+  ConsumerState<NavbarButtonsWidget> createState() =>
+      _NavbarButtonsWidgetState();
+}
+
+class _NavbarButtonsWidgetState extends ConsumerState<NavbarButtonsWidget> {
+  List<bool> isHovered = List.generate(5, (index) => false);
+
+  @override
+  Widget build(BuildContext context) {
+    double maxWidth = MediaQuery.of(context).size.width;
+    double maxHeight = MediaQuery.of(context).size.height;
+
+    final List<String> socialImages = [
+      'assets/icons/linkedin-svgrepo-com.svg',
+      'assets/icons/github-svgrepo-com.svg',
+      'assets/icons/leetcode-svgrepo-com.svg',
+      'assets/icons/twitter-svgrepo-com.svg',
+      'assets/icons/instagram-svgrepo-com.svg'
+    ];
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        NavButton(
-          title: 'About',
-          onTap: () {
-            ref.read(selectedIndexProvider.notifier).state = 0;
-          },
-          isSelected: selectedIndex == 0,
-        ),
-        NavButton(
-            title: 'Projects',
+        // Navigation Buttons
+        for (int i = 0; i < 3; i++)
+          NavButton(
+            title: ['About', 'Projects', 'Contact'][i],
             onTap: () {
-              ref.read(selectedIndexProvider.notifier).state = 1;
+              ref.read(selectedIndexProvider.notifier).state = i;
             },
-            isSelected: selectedIndex == 1),
-        NavButton(
-            title: 'Connect',
-            onTap: () {
-              ref.read(selectedIndexProvider.notifier).state = 2;
-            },
-            isSelected: selectedIndex == 2),
+            isSelected: widget.selectedIndex == i,
+          ),
+
         const Padding(
           padding: EdgeInsets.symmetric(horizontal: 12.0),
           child: Divider(
             color: AppColors.light,
             thickness: 2,
           ),
-        )
+        ),
+
+        // Social Icons
+        Padding(
+          padding: EdgeInsets.only(top: maxHeight * 0.02),
+          child: Wrap(
+            spacing: maxWidth * 0.02,
+            runSpacing: maxHeight * 0.02,
+            alignment: WrapAlignment.center,
+            children: List.generate(socialImages.length, (index) {
+              return MouseRegion(
+                onEnter: (_) => setState(() => isHovered[index] = true),
+                onExit: (_) => setState(() => isHovered[index] = false),
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 200),
+                  padding: EdgeInsets.all(isHovered[index] ? 10 : 5),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    boxShadow: isHovered[index]
+                        ? [
+                            BoxShadow(
+                              color: AppColors.light.withOpacity(0.5),
+                              blurRadius: 10,
+                              spreadRadius: 5,
+                            )
+                          ]
+                        : [],
+                  ),
+                  child: SvgPicture.asset(
+                    socialImages[index],
+                    height: maxHeight * 0.06,
+                    width: maxHeight * 0.06,
+                    fit: BoxFit.contain,
+                    colorFilter: isHovered[index]
+                        ? const ColorFilter.mode(
+                            AppColors.light, BlendMode.srcIn)
+                        : null,
+                  ),
+                ),
+              );
+            }),
+          ),
+        ),
+
+        SizedBox(height: maxHeight * 0.05),
       ],
     );
   }
