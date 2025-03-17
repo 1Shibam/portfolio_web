692d79220cf632dab800226aeb99132bd12a379b
1Shibam <hehe.shibam@gmail.com>
2025-03-17 21:20:39 +0530

    loads data in the project section

--- lib/responsive/desktop/project_section_widget.dart
+++ lib/responsive/desktop/project_section_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:portfolio_web/helper/data_provider.dart';
 import 'package:portfolio_web/theme/text_styles.dart';
 import 'package:portfolio_web/widgets/section_divide_widget.dart';
 
@@ -42,26 +44,54 @@ class _ProjectSectionWidgetState extends State<ProjectSectionWidget> {
           ),
           Expanded(
             child: Scrollbar(
-              interactive: true,
-              controller: _scrollController,
-              thickness: maxWidth * 0.01,
-              thumbVisibility: true, // Always show the scrollbar
-              trackVisibility: true,
-              child: ListView.builder(
+                interactive: true,
                 controller: _scrollController,
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: EdgeInsets.only(right: 0.02 * maxWidth),
-                    child: SectionDividerWidget(
-                        maxWidth: maxWidth,
-                        maxHeight: maxHeight,
-                        title1: 'video Demo',
-                        title2: 'Tech-Stacks'),
-                  );
-                },
-              ),
-            ),
+                thickness: maxWidth * 0.01,
+                thumbVisibility: true, // Always show the scrollbar
+                trackVisibility: true,
+                child: Consumer(
+                  builder: (context, ref, child) {
+                    final userData = ref.watch(userDataStateNotifierProvider);
+                    return userData.when(
+                        data: (data) {
+                          final projects = data.projects;
+                          /*
+                          {
+      "name": "AI-Powered Chatbot App",
+      "description": "A chatbot with real-time responses, image-based answers, and read-aloud functionality, using Firebase and Chat GPT API.",
+      "technologies": ["Flutter", "Dart", "AI Models", "Firebase", "Backend APIs"],
+      "github": "https://github.com/yourusername/chatbot-app",
+      "demo": "https://link/to/the/video"
+    }
+                           */
+                          return ListView.builder(
+                            controller: _scrollController,
+                            itemCount: projects.length,
+                            itemBuilder: (context, index) {
+                              final singleProj = projects[index];
+                              return Padding(
+                                padding:
+                                    EdgeInsets.only(right: 0.02 * maxWidth),
+                                child: SectionDividerWidget(
+                                    projectDesc: singleProj.description,
+                                    projectName: singleProj.name,
+                                    techStacks: singleProj.technologies,
+                                    maxWidth: maxWidth,
+                                    maxHeight: maxHeight,
+                                    section1: 'video Demo',
+                                    section2: 'Tech-Stacks'),
+                              );
+                            },
+                          );
+                        },
+                        error: (error, stackTrace) => const Center(
+                              child: Text('Something went wrong sorry!!'),
+                            ),
+                        loading: () => const Center(
+                              child: CircularProgressIndicator(),
+                            ));
+                  },
+                )),
           )
               .animate()
               .fade(duration: 1000.ms)
