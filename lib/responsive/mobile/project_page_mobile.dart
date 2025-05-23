20f91528c8efcfce1c85da5acc016dff24d3fcbf
1Shibam <hehe.shibam@gmail.com>
2025-05-23 11:42:27 +0530

    add project page for mobile view

---
+++ lib/responsive/mobile/project_page_mobile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProjectPageMobile extends ConsumerStatefulWidget {
+  const ProjectPageMobile({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _ProjectPageMobileState();
+}
+
+class _ProjectPageMobileState extends ConsumerState<ProjectPageMobile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
